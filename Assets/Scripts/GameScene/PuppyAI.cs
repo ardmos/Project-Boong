e6605c386a25e01237e9e890503c41a7fd10c9fc
ardmos <46891528+ardmos@users.noreply.github.com>
2024-03-16 23:27:58 +0900

    Feat : Puppy AI(스크립트 기반)에 NavMeshAgent 적용

--- Assets/Scripts/GameScene/PuppyAI.cs
+++ Assets/Scripts/GameScene/PuppyAI.cs
@@ -1,4 +1,6 @@
+using System.Linq.Expressions;
 using UnityEngine;
+using UnityEngine.AI;
 
 public enum State
 {
@@ -10,12 +12,21 @@ public class PuppyAI : MonoBehaviour
 {
     public State currentState = State.Patrol;
     public Transform[] patrolPoints;
-    public float chaseSpeed = 5f;
+    public float chaseSpeed = 10f;
     public float patrolSpeed = 3f;
     public Transform player;
     public float chaseDistance = 10f;
 
     private int currentPatrolIndex = 0;
+
+    [SerializeField] private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
 
     private void Update()
     {
@@ -33,7 +44,9 @@ public class PuppyAI : MonoBehaviour
     private void Patrol()
     {
         // Move towards the current patrol point
-        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currentPatrolIndex].position, patrolSpeed * Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currentPatrolIndex].position, patrolSpeed * Time.deltaTime);
+        agent.speed = patrolSpeed;
+        MovePuppy(patrolPoints[currentPatrolIndex].position);
 
         // Check if reached the patrol point
         if (Vector3.Distance(transform.position, patrolPoints[currentPatrolIndex].position) < 0.1f)
@@ -52,12 +65,19 @@ public class PuppyAI : MonoBehaviour
     private void Chase()
     {
         // Move towards the player
-        transform.position = Vector3.MoveTowards(transform.position, player.position, chaseSpeed * Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, player.position, chaseSpeed * Time.deltaTime);
+        agent.speed = chaseSpeed;
+        MovePuppy(player.position);
 
         // Check if the player is out of chase distance
         if (Vector3.Distance(transform.position, player.position) > chaseDistance)
         {
             currentState = State.Patrol;
         }
+    }
+
+    private void MovePuppy(Vector3 targetPos)
+    {
+        agent.SetDestination(targetPos);
     }
 }
