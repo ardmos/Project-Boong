8df4b936cc184ea8f3cd941fdb7a680619c667cc
ardmos <46891528+ardmos@users.noreply.github.com>
2024-04-04 01:01:27 +0900

    Test : BGM 구현 테스트중 중간 저장.

---
+++ Assets/Scripts/BGMManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+    public AudioClip bgmTitleScene;
+    public AudioClip bgmGameScene;
+}
